aa6aa4db591f98e5e84c7be65df0bcab70edae25
msnodeve <msnodeve@gmail.com>
2024-08-05 23:32:22 +0900

    board Entity 생성 및 mysql 로컬 추가

---
+++ src/main/java/com/board/api/entity/Board.java
@@ -0,0 +1,25 @@
+package com.board.api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@Table(name = "boards")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", columnDefinition = "varchar(255)", nullable = false)
+    private String title;
+
+    @Column(name = "content", columnDefinition = "text", nullable = false)
+    private String content;
+}
